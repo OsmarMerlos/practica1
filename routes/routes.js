85505493c5af4f986843a0e659aa9cda7f0d32be
Osmar <demi.gody666@gmail.com>
2021-03-04 10:08:32 -0400

    practice

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,11 @@
 import express from "express";
 import IndexController from "../controller/indexController.js";
 import TodoController from "../controller/todoController.js";
-import UserController from "../controller/userController.js";
 import RolesController from "../controller/rolesContoller.js";
 import JsonWebTokenManagement from "../middleware/JsonWebTokenManagement.js";
 
 var router = express.Router();
 var indexControler = new IndexController();
-var userController = new UserController();
 var rolesController = new RolesController();
 var jsonwebtokenmanagement = new JsonWebTokenManagement();
 
@@ -22,14 +20,7 @@ router.post("/login", indexControler.login);
 /**
  * SERVICIO PROTEGIDO
  */
-router.get("/user", userController.getUsers);
-/* FIN DE SERVICIO PROTEGIDO */
-router.post("/user", userController.createUser);
-router.put("/user/:id", userController.updateUser);
-router.delete("/user/:id", userController.deleteUser);
-router.post("/addRol", userController.addRol);
-router.post("/uploadAvatar/:id", userController.uploadAvatar);
-router.get("/showavatar/:name", userController.getAvatar);
+
 /* GET home page. */
 /**
  * Endpoints de los usuarios
